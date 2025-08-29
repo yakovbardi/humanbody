e91dfb27fb273f843760d619f6e4ffd3f9e6f293
yakovbardi <yakovbardi@gmail.com>
2025-08-28 21:22:41 -0400

    deleting planet column, for 'twas made on wrong branch, being that it was made on MAIN

--- 1 table/1 table.sql
+++ 1 table/1 table.sql
@@ -4,7 +4,6 @@ drop table if exists dbo.body
 go
 create table dbo.body(
     id int not null identity primary key, 
-    planet varchar(60) not null,
     bodypart varchar(100) not null,
     num int null
 )
