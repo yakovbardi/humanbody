d78254588970f886c73cff2853e9cb426a01dca4
yakovbardi <yakovbardi@gmail.com>
2025-08-29 11:33:07 -0400

    Update 2 sample data.sql

--- 2 sample data/2 sample data.sql
+++ 2 sample data/2 sample data.sql
@@ -2,7 +2,7 @@ insert body(bodypart, num)
 select 'eyes', 2
 union select 'ears', 2
 union select 'nose', 1
-union select 'mouth', 1
+union select 'muoth', 1
 union select 'teeth', 32
 union select 'tounue', 1
 union select 'hair', null
