55cdddf207b268afd519b8ac59d6cdd3cd1088c7
yakovbardi <yakovbardi@gmail.com>
2025-11-24 15:51:56 -0500

    1 table+2 sample data11-24-2025

--- 1 table/1 table.sql
+++ 1 table/1 table.sql
@@ -2,8 +2,4 @@ use humanbodyDB
 go
 drop table if exists dbo.body
 go
-create table dbo.body(
-    id int not null identity primary key, 
-    bodypart varchar(100) not null,
-    num int null
-)
+create table dbo.body(id int not null identity primary key, bodypart varchar(100) not null, num int not null)
--- 2 sample data/2 sample data.sql
+++ 2 sample data/2 sample data.sql
@@ -2,7 +2,7 @@ insert body(bodypart, num)
 select 'eyes', 2
 union select 'ears', 2
 union select 'nose', 1
-union select 'mouth', 1
+union select 'mouth', 2
 union select 'teeth', 32
-union select 'tounue', 1
-union select 'hair', null
+insert body(bodypart, num)
+select 'toungue', 1
